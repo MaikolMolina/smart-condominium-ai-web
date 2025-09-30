2c29b37bd7e5f32821c5201d2557418327c078c4
MaikolMolina <MolinaMaikol73@gmail.com>
2025-09-29 22:03:52 -0400

    c

---
+++ backend/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit  # Detenerse si hay un error
+
+# 1️⃣ Instalar dependencias desde requirements.txt en la raíz
+pip install -r ../requirements.txt
+
+# 2️⃣ Recolectar archivos estáticos de Django
+python manage.py collectstatic --no-input
+
+# 3️⃣ Aplicar migraciones
+python manage.py migrate
